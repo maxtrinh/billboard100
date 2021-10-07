0ca8b82e52b74ccb7273e29e14ecaa406a1fd3dd
Max Trinh <maxtnyc10@gmail.com>
2021-10-07 16:20:24 -0400

    pivot longer

--- billboard.R
+++ billboard.R
@@ -13,9 +13,14 @@ billboard <- read_csv('https://clanfear.github.io/CSSS508/Lectures/Week5/data/bi
 
 billboard <- read_csv('https://clanfear.github.io/CSSS508/Lectures/Week5/data/billboard.csv', guess_max = 5000)
 
+# Pivot longer 
+df <- billboard %>% 
+    pivot_longer(cols = starts_with('wk'),
+                 names_to = 'week',
+                 values_to = 'rank',
+                 values_drop_na = T)
 
-
-
+dim(df)
 
 
 
